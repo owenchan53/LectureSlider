8915ff30c7aa98ff7e4aaf6541460757f5e1d999
Owen Chan <owenchanchaktao@gmail.com>
2024-10-17 19:30:07 -0400

    Updated setttings.py

--- lectureslider/lectureslider/settings.py
+++ lectureslider/lectureslider/settings.py
@@ -20,6 +20,7 @@ MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "/media/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
